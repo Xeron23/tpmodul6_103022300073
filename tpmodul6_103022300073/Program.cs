86155066dd45a9e749777bdb5cbb4d805a6b6de1
Aria Lent <kaqyword2304@gmail.com>
2025-03-23 17:10:11 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300073/Program.cs
+++ tpmodul6_103022300073/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+    private Random random = new Random();
+    public SayaTubeVideo(string title)
+    {
+        this.title = title;
+        playCount = 0;
+        id = random.Next(10000, 100000);
+    }
 
+    public void IncreasePlayCount(int play)
+    {
+        playCount += play;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("id: " + id + ", Title: " + title + ", play: " + playCount);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - [Muhammad Rifki Anandita]");
+        video1.IncreasePlayCount(10);
+        video1.PrintVideoDetails();
+    }
+}
