b92aeee562844c6593225730795b4c5087a1c731
Aria Lent <kaqyword2304@gmail.com>
2025-03-23 17:54:18 +0700

    implementasi design by contract

--- tpmodul6_103022300073/Program.cs
+++ tpmodul6_103022300073/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 public class SayaTubeVideo
 {
     private int id;
@@ -7,6 +8,8 @@ public class SayaTubeVideo
     private Random random = new Random();
     public SayaTubeVideo(string title)
     {
+        Debug.Assert(!string.IsNullOrEmpty(title), "Judul video tidak boleh null atau kosong.");
+        Debug.Assert(title.Length <= 100, "Judul video tidak boleh lebih dari 100 karakter.");
         this.title = title;
         playCount = 0;
         id = random.Next(10000, 100000);
@@ -14,7 +17,20 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int play)
     {
-        playCount += play;
+        Debug.Assert(play > 0 && play <= 10000000, "Play count harus di antara 1 hingga 10.000.000.");
+        try
+        {
+
+            checked
+            {
+                playCount += play;
+            }
+
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Play count melebihi batas integer maksimum!");
+        }
     }
 
     public void PrintVideoDetails()
@@ -27,8 +43,35 @@ public class Program
 {
     public static void Main()
     {
-        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - [Muhammad Rifki Anandita]");
-        video1.IncreasePlayCount(10);
-        video1.PrintVideoDetails();
+        try
+        {
+            SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract - [Muhammad Rifki Anandita]");
+            video1.IncreasePlayCount(10);
+
+
+            SayaTubeVideo video2 = new SayaTubeVideo("Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]Tutorial Design By Contract - [Muhammad Rifki Anandita]");
+
+            //SayaTubeVideo video3 = new SayaTubeVideo(null);
+
+
+            video1.IncreasePlayCount(20000000);
+
+            for (int i = 0; i < 10; i++)
+            {
+                video1.IncreasePlayCount(int.MaxValue);
+            }
+
+
+
+
+
+            video1.PrintVideoDetails();
+        }
+        catch (Exception e)
+        {
+
+        }
+        
+
     }
 }
